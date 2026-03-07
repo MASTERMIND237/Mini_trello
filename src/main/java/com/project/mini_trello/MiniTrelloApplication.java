860e510ff874ec5f7c69daaedff1cd59eff1adcf
MASTERMIND237 <essonguemarcien25@gmail.com>
2026-03-07 18:22:03 +0100

    first commit

---
+++ src/main/java/com/project/mini_trello/MiniTrelloApplication.java
@@ -0,0 +1,13 @@
+package com.project.mini_trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTrelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mini_trello/MiniTrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mini_trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
